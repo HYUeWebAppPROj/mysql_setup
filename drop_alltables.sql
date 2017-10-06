2a06cc7377afcf97c0c8abdd771b6b4936f5e70b
hsh97Erica <imscs21@hanyang.ac.kr>
2017-10-06 16:23:24 +0900

    :P

---
+++ drop_alltables.sql
@@ -0,0 +1,9 @@
+use waproj;
+drop table if exists course_storage;
+drop table if exists notice_board;
+drop table if exists user_registered_courses;
+drop table if exists course_detail_list;
+drop table if exists course_list;
+drop table if exists user_storage;
+drop table if exists userlist;
+drop table if exists loginserviceprovider;
